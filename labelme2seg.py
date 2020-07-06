2e9c71ec0c8fbe03760dace7d831b2432ddcb172
woods <woodsgao@outlook.com>
2020-07-06 21:23:25 +0800

    fix

--- labelme2seg.py
+++ labelme2seg.py
@@ -8,30 +8,30 @@ import cv2
 from tqdm import tqdm
 
 
-def labelme2seg(src_dir, dst_dir):
+def labelme2seg(labelme_dir, output_dir):
     classes = []
-    img_dir = osp.join(dst_dir, 'images')
-    label_dir = osp.join(dst_dir, 'labels')
-    os.makedirs(dst_dir, exist_ok=True)
+    img_dir = osp.join(output_dir, 'images')
+    label_dir = osp.join(output_dir, 'labels')
+    os.makedirs(output_dir, exist_ok=True)
     os.makedirs(img_dir, exist_ok=True)
     os.makedirs(label_dir, exist_ok=True)
-    names = os.listdir(src_dir)
+    names = os.listdir(labelme_dir)
     names = [n for n in names if osp.splitext(n)[-1] == '.json']
     for name in names:
-        name = osp.join(src_dir, name)
+        name = osp.join(labelme_dir, name)
         os.system('labelme_json_to_dataset %s && rm %s' % (name, name))
-    names = os.listdir(src_dir)
-    names = [n for n in names if osp.isdir(osp.join(src_dir, n))]
+    names = os.listdir(labelme_dir)
+    names = [n for n in names if osp.isdir(osp.join(labelme_dir, n))]
     for name in tqdm(names):
-        shutil.move(osp.join(src_dir, name, 'label.png'),
-                                osp.join(label_dir, name[:-5] + '.png'))))
-        shutil.move(osp.join(src_dir, name, 'img.png'),
-                                osp.join(img_dir, name[:-5] + '.png'))))
+        shutil.move(osp.join(labelme_dir, name, 'label.png'),
+                                osp.join(label_dir, name[:-5] + '.png'))
+        shutil.move(osp.join(labelme_dir, name, 'img.png'),
+                                osp.join(img_dir, name[:-5] + '.png'))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('src', type=str, help='src file path')
-    parser.add_argument('dst', type=str, help='dst file path')
+    parser.add_argument('labelme', type=str, help='labelme file path')
+    parser.add_argument('output', type=str, help='output file path')
     opt = parser.parse_args()
-    labelme2seg(opt.src, opt.dst)
+    labelme2seg(opt.labelme, opt.output)
